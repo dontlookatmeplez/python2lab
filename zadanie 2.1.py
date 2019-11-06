4a00d7b37279b8dd3daf3ccfc24b6fc5d38bdc24
dontlookatmeplez <35170040+dontlookatmeplez@users.noreply.github.com>
2019-11-06 14:09:14 -0800

    Add files via upload

---
+++ zadanie 2.1.py
@@ -0,0 +1,10 @@
+print("This utility will show you whether a integer is an odd or even number.")
+print("######################################################################")
+num = int(input("To continue enter a integer: "))
+    #allows you to put a value that is a integer, and then makes use of it
+if (num % 2) == 0:
+    #divides your integer by 2 and shows if remainder is equal to 0
+    print("{0} is even".format(num))
+    #{0} together with .format(***) allows you to use your values in brackets
+else:
+    print("{0} is odd".format(num))
---
+++ zadanie 2.2.py
@@ -0,0 +1,12 @@
+num_a = float(input("Enter value for a: "))
+num_x = float(input("Enter value for x: "))
+num_b = float(input("Enter value for b: "))
+num_y = float(0)
+if num_a == 0:
+    print("Don't divide by zero")
+    #helps to avoid error when dividing by zero
+else:
+    print("Zero of a function is equal to:", -num_b / num_a)
+
+
+
