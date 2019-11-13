12bf71e8a837c1bacd864ab525958e09ac63dfce
dontlookatmeplez <35170040+dontlookatmeplez@users.noreply.github.com>
2019-11-13 16:22:13 +0100

    Update zadanie 2.5.py

--- zadanie 2.5.py
+++ zadanie 2.5.py
@@ -1,17 +1,22 @@
 while True:
   try:
-    mark = int(input("Enter a percentage:"))
-    if 100 > mark > 90:
+    mark = int(input("Enter a percentage: "))
+    if mark > 100:
+      print("You can score as much as 100%!")
+    elif 100 >= mark >= 90:
       print("You would score A!")
-    elif 89 > mark > 79:
+    elif 89 >= mark >= 79:
       print("You would score B+!")
-    elif 78 > mark > 68:
+    elif 78 >= mark >= 68:
       print("You would score B!")
-    elif 67 > mark > 57:
+    elif 67 >= mark >= 57:
       print("You would score C+")
-    elif 56 > mark > 0:
+    elif 56 >= mark >= 46:
+      print("You would score C!")
+    elif 45 >= mark >= 0:
       print("You would not pass...")
       
   except ValueError:
-    print("Enter a valid value.")
+    print("Enter a valid value (0-100)")
+
 
