d4c50400c6a87ec401c29a5540b428a2cf2e5369
dontlookatmeplez <35170040+dontlookatmeplez@users.noreply.github.com>
2019-11-13 15:25:38 +0100

    Update zadanie 2.1.py

--- zadanie 2.1.py
+++ zadanie 2.1.py
@@ -5,6 +5,6 @@ num = int(input("To continue enter a integer: "))
 if (num % 2) == 0:
     #divides your integer by 2 and shows if remainder is equal to 0
     print("{0} is even".format(num))
-    #{0} together with .format(***) allows you to use your values in brackets
+    #{0} together with .format(***) allows you to use your defined (as "num") values in brackets
 else:
     print("{0} is odd".format(num))
