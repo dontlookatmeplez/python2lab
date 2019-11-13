978da70cf39685e6e8f9dfb601e9b5f1fc3631b4
dontlookatmeplez <35170040+dontlookatmeplez@users.noreply.github.com>
2019-11-13 16:09:13 +0100

    Create zadanie 2.5.py

---
+++ zadanie 2.5.py
@@ -0,0 +1,15 @@
+try:
+  mark = int(input("Enter a percentage:"))
+  if 100 > mark > 90:
+    print("You would score A!")
+  elif 89 > mark > 79:
+    print("You would score B+!")
+  elif 78 > mark > 68:
+    print("You would score B!")
+  elif 67 > mark > 57:
+    print("You would score C+")
+  elif 56 > mark > 0:
+    print("You would not pass...")
+    
+except ValueError:
+  print("xd")
