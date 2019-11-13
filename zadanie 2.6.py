7fcef7849a2cb4adc52a3ae51b77ca1af5a92107
dontlookatmeplez <35170040+dontlookatmeplez@users.noreply.github.com>
2019-11-13 16:33:27 +0100

    Create zadanie 2.6.py

---
+++ zadanie 2.6.py
@@ -0,0 +1,9 @@
+while True:
+  try:
+    x = input(int("Enter a coordinate X: "))
+    y = input(int("Enter a coordinate Y: "))
+    if x > -1 and y > 1:
+      print("xsadsada")
+except:
+print("xsda")
+     
